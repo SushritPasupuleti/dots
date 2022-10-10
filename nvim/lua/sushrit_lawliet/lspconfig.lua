1be8966dfef0657121d8b452f0d3d7c378505569
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2022-10-10 19:56:42 +0530

    feat: fix formatting

--- nvim/lua/sushrit_lawliet/lspconfig.lua
+++ nvim/lua/sushrit_lawliet/lspconfig.lua
@@ -52,7 +52,7 @@ local on_attach = function(client, bufnr)
 	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
 	vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
 	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-	-- vim.keymap.set("n", "<space>f", vim.lsp.buf.format(), bufopts)
+	vim.keymap.set("n", "<space>f", vim.lsp.buf.format, bufopts)
 end
 
 for _, lsp in ipairs(servers) do
