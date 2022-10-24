40d62f77f6cbf9a08b0c78f6fabd02ef2f64e18d
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2022-10-24 09:36:31 +0530

    feat: switch to sane defaults for fish and fisher

--- fish/completions/dl.fish
+++
@@ -1,3 +0,0 @@
-complete --command dl --description "Wrapper around youtube-dl to easily download media"
-complete --command dl --short h --long help --description "Display help"
-complete --command dl --short v --long version --description "Display version"
