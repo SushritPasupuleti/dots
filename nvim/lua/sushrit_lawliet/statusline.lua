95da8a76267033a6b4e0956ff0bd140c588a941a
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2023-01-05 08:52:47 +0530

    feat: update separators

--- nvim/lua/sushrit_lawliet/statusline.lua
+++ nvim/lua/sushrit_lawliet/statusline.lua
@@ -16,8 +16,10 @@ require("lualine").setup({
 	options = {
 		icons_enabled = true,
 		theme = "auto",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
+		-- component_separators = { left = "", right = "" },
+		component_separators = { left = "", right = "" },
+		-- section_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
 		disabled_filetypes = {
 			statusline = {},
 			winbar = {},
