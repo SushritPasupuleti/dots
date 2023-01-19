ca4bb1e91dea5e252fc81d24eb3024670dcf3f1e
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2023-01-19 10:20:37 +0530

    feat: upgrade prompt and fish

--- fish/config.fish
+++ fish/config.fish
@@ -14,16 +14,23 @@ set PATH $ANDROID_HOME/tools/bin $PATH
 set PATH $ANDROID_HOME/platform-tools $PATH
 set PATH /usr/local/opt/libpq/bin $PATH
 set PATH $HOME/.cargo/bin $PATH
+fish_add_path /opt/homebrew/sbin
 
 # fzf
 
 set fzf_preview_dir_cmd exa --all --color=always
-set fzf_preview_file_cmd bat
+set fzf_preview_file_cmd bat --color=always
 set fzf_fd_opts --hidden --exclude=.git
+
+# Alias
+alias ls='exa --all --color=always'
+alias lg='lazygit'
 
 fzf_configure_bindings --directory=\cf --git_log=\cl --git_status=\cs --processes=\cp
 
 # ggl - for googling
 
 alias gg="ggl -b='Microsoft Edge'"
+
+zoxide init fish | source
 starship init fish | source
--- fish/functions/lg.fish
+++ fish/functions/lg.fish
@@ -1,3 +1,4 @@
-function lg --wraps='exa $EXA_STANDARD_OPTIONS $EXA_LG_OPTIONS' --description 'alias lg exa $EXA_STANDARD_OPTIONS $EXA_LG_OPTIONS'
-  exa $EXA_STANDARD_OPTIONS $EXA_LG_OPTIONS $argv; 
+function lg --wraps=lazygit --wraps='exa $EXA_STANDARD_OPTIONS $EXA_LG_OPTIONS' --description 'alias lg exa $EXA_STANDARD_OPTIONS $EXA_LG_OPTIONS'
+  exa $EXA_STANDARD_OPTIONS $EXA_LG_OPTIONS $argv
+        
 end
