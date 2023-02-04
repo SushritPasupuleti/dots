5f1702d9362d6b8deb46af9e9a5f912d21eb0fa2
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2023-02-04 19:00:12 +0530

    fix: linux support

--- pre-setup.sh
+++ pre-setup.sh
@@ -23,7 +23,7 @@ function info {
 	printf "${BLUE}$@${NC}\n"
 }
 
-if [[ "$OSTYPE" =~ ^darwin ]];
+if [[ "$OSTYPE" =~ ^darwin ]] || [[ "$OSTYPE" =~ ^linux ]];
 then
 	if ! command -v brew &> /dev/null
 	then
@@ -58,49 +58,7 @@ then
 		   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 else
-	if [[ "$OSTYPE" =~ ^linux ]]
-
-	# Get Distro (debian, arch, etc)
-	# For future reference
-	# awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }'
-	
-	then
-	echo $(warn "Linux support is not perfect (varies from distro to distro)")
-	if ! command -v brew &> /dev/null
-	then
-		echo $(warning "brew could not be found")
-
-		read -p "Would you like to install it? (y/n): " -n 1 -r
-			echo    # (optional) move to a new line
-			if [[ ! $REPLY =~ ^[Yy]$ ]]
-			then
-				exit 1
-		fi
-
-		# Install Homebrew
-		
-		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-		exit
-	fi
-
-
-	brew install git gh bat nvm tmux nvim ctags lazygit ranger vifm emacs git-delta starship
-	brew install --cask iterm2 visual-studio-code keycastr obs kicad
-	brew install fzt czg add-gitignore grex undollar has grip tldr how2 exa fd zoxide get-port epy timg mdlt luarocks jira-cli newman oha gping gtm gnu-sed jq glow fd cmatrix pipes-sh neofetch openjdk ruby terminal-notifier rust rust-analyzer ffmpeg prettier wget shellcheck urlview go gum
-	brew tap jabley/homebrew-wrk2
-	brew install --HEAD wrk2
-
-	echo $(success "Done installing packages.")
-
-	echo $(info "Installing vim-plug")
-
-	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-		   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-		exit 1
-	else
-		echo $(error "Unsupported OS. Please use Linux or MacOS.")
-		echo $(error "Note For Windows Users: No Plans to support Windows")
-		exit 1
-	fi
+	echo $(error "Unsupported OS. Please use Linux or MacOS.")
+	echo $(error "Note For Windows Users: No Plans to support Windows")
+	exit 1
 fi
