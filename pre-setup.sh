e3ff7cb418b3b5719c2ba90c25bd21f4c1b59739
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2023-02-04 19:52:20 +0530

    Update pre-setup.sh

--- pre-setup.sh
+++ pre-setup.sh
@@ -44,7 +44,7 @@ then
 	fi
 
 
-	brew install git gh bat nvm tmux nvim ctags lazygit ranger vifm emacs git-delta starship
+	brew install git gh bat nvm tmux nvim ctags lazygit ranger vifm emacs git-delta starship fish
 	brew install --cask iterm2 visual-studio-code keycastr obs kicad
 	brew install fzf czg add-gitignore grex undollar has grip tldr how2 exa fd zoxide get-port epy timg mdlt luarocks jira-cli newman oha gping gnu-sed jq glow fd cmatrix pipes-sh neofetch openjdk ruby terminal-notifier rust rust-analyzer ffmpeg prettier wget shellcheck urlview go gum
 	brew tap jabley/homebrew-wrk2
