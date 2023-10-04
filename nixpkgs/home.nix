b567bdcc301aa1f17e7b9213e167cc026f5da958
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2023-10-04 10:12:53 +0530

    feat: add home-manager config

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -1,5 +1,6 @@
 # home.nix
-{ pkgs
+{ config
+, pkgs
 , ...
 }: {
   gtk = {
@@ -14,4 +15,19 @@
       };
     };
   };
+  home.packages = [
+    pkgs.bun
+  ];
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
 }
