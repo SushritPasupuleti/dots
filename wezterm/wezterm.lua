bcb019d10c6bfe226c2650a55fd276dc16bd74e7
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2024-01-28 09:23:00 +0530

    feat: start wezterm in maximized state

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,12 @@
 local wezterm = require("wezterm")
 
+local mux = wezterm.mux
+
+wezterm.on("gui-startup", function()
+  local tab, pane, window = mux.spawn_window{}
+  window:gui_window():maximize()
+end)
+
 return {
 	enable_wayland = true,
 	-- front_end = 'WebGpu', -- To work with Wayland + Nvidia
