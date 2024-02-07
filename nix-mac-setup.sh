d028271b04742bfc624b4a48cc510ccd98d8b58c
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2024-02-07 07:29:24 +0530

    feat: add flake for mac

---
+++ nix-mac-setup.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# --- Core ---
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;36m'
+NC='\033[0m' # No Color
+
+function error {
+    printf "${RED}$@${NC}\n"
+}
+
+function success {
+    printf "${GREEN}$@${NC}\n"
+}
+
+function warn {
+    printf "${YELLOW}$@${NC}\n"
+}
+
+function info {
+	printf "${BLUE}$@${NC}\n"
+}
+
+cp -fr "$(pwd -P)"/nixos/hosts/mac/flake.nix ~/.config/nix/
+
+echo $(success "Finished copying flake.nix to ~/.config/nix")
+echo 
+echo $(info "Run \`nix profile install .\` to install the flake for the first time")
+echo $(info "Run \`nix profile upgrade 0\` to upgrade the existing flake")
---
+++ nixos/hosts/mac/flake.nix
@@ -0,0 +1,151 @@
+# flake.nix skeleton:
+
+{
+  description = "MacOS Flake";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, darwin }: {
+    packages."aarch64-darwin".default =
+      # let pkgs = nixpkgs.legacyPackages."aarch64-darwin";
+      let
+        pkgs = import nixpkgs {
+          system = "aarch64-darwin";
+          config.allowUnfree = true;
+        };
+      in pkgs.buildEnv {
+        name = "home-packages";
+        paths = with pkgs; [
+          # general tools
+          git
+          gh
+          neovim
+          vim
+          tmux
+          zellij
+          ranger
+          lazygit
+          lazydocker
+          delta
+          neofetch
+          starship
+          nerdfonts
+          # langs
+          go
+          golangci-lint
+          python311
+          python311Packages.pip
+          nodejs_18
+          elixir_1_15
+          elixir-ls
+          ocaml
+          opam
+          ocamlPackages.findlib
+          ocamlformat
+          kotlin
+          kotlin-native
+          kotlin-language-server
+          ktlint
+          spring-boot-cli
+          # dotnet-sdk_7
+          # dotnet-runtime_7
+          # dotnet-aspnetcore_7
+          # grpc-tools
+          grpcurl
+          grpcui
+          protobuf3_20
+          # nodePackages.eas-cli
+          nodePackages.tailwindcss
+          nodePackages.pnpm
+          nodePackages_latest.eslint
+          bun # <--- use latest
+          yarn
+          gcc
+          rustup
+          # rust-analyzer
+          sqlx-cli
+          lua
+          luajitPackages.luacheck
+          stylua
+          openjdk17
+          #utils
+          lsof
+          gum
+          glow
+          eza
+          zoxide
+          fzf
+          fd
+          ctags
+          gnused
+          ripgrep
+          nixfmt
+          deadnix
+          checkmake
+          shellcheck
+          sqlfluff
+          dotenv-linter
+          unzip
+          fselect
+          sioyek
+          nixos-generators
+          # etcher
+          # wrk2
+          bat
+          btop
+          htop
+          # nvitop
+          # python310Packages.gpustat
+          python311Packages.pyspark
+          sqlite
+          # neofetch
+          inxi
+          timg
+          # appeditor
+          git-ignore
+          #cli
+          marp-cli
+          aws-sam-cli
+          awscli2
+          aws-iam-authenticator
+          terraform
+          terraform-ls
+          terraform-docs
+          # k6
+          pkg-config
+          wget
+          # urlview
+          mqttui
+          jq
+          fx
+          cloc
+          fish
+          fishPlugins.done
+          fishPlugins.fzf-fish
+          # Fancy Eye Candy
+          pipes-rs
+          cbonsai
+          cmatrix
+          # tools
+          # spark
+          docker
+          # k3s
+          ktunnel
+          # apache-airflow
+          airlift # local airflow with containers
+          datree
+          kind # local docker clusters
+          kubectl
+          kubectx
+          k9s
+          kubernetes-helm
+          # minikube
+          #github-desktop
+          gnumake
+        ];
+      };
+  };
+}
